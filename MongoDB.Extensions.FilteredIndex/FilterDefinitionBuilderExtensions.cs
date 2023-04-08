ae1096e5061d0de6abc43a1fb53110fb3281b6f6
William Wolfram <willwolfram18@gmail.com>
2023-04-07 22:34:08 -0700

    Basic use case for array filter definition

---
+++ MongoDB.Extensions.FilteredIndex/FilterDefinitionBuilderExtensions.cs
@@ -0,0 +1,57 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace MongoDB.Extensions.FilteredIndex
+{
+    public static class FilterDefinitionBuilderExtensions
+    {
+        public static ArrayFilterDefinition<TSource> ItemEq<TSource>(this FilterDefinitionBuilder<TSource> builder,
+            string identifier,
+            TSource value)
+        {
+            IBsonSerializer<TSource> valueSerializer = BsonSerializer.LookupSerializer<TSource>();
+            var document = new BsonDocument();
+            using (var writer = new BsonDocumentWriter(document))
+            {
+                var context = BsonSerializationContext.CreateRoot(writer);
+                writer.WriteStartDocument();
+                writer.WriteName(identifier);
+                valueSerializer.Serialize(context, value);
+                writer.WriteEndDocument();
+            }
+
+            return document;
+        }
+
+        public static ArrayFilterDefinition<TSource> ItemGte<TSource>(this FilterDefinitionBuilder<TSource> builder,
+            string identifier,
+            TSource value)
+        {
+            IBsonSerializer<TSource> valueSerializer = BsonSerializer.LookupSerializer<TSource>();
+            var document = new BsonDocument();
+            using (var writer = new BsonDocumentWriter(document))
+            {
+                var context = BsonSerializationContext.CreateRoot(writer);
+                writer.WriteStartDocument();
+                writer.WriteName(identifier);
+                writer.WriteStartDocument();
+                writer.WriteName("$gte");
+                valueSerializer.Serialize(context, value);
+                writer.WriteEndDocument();
+                writer.WriteEndDocument();
+            }
+
+            return document;
+        }
+
+        public static ArrayFilterDefinition<TSource> foo<TSource>(this FilterDefinitionBuilder<TSource> builder,
+            string identifier,
+            FilterDefinition<TSource> filter)
+        {
+            var x = filter.Render(BsonSerializer.LookupSerializer<TSource>(), BsonSerializer.SerializerRegistry);
+            throw new NotImplementedException();
+        }
+    }
+}
--- MongoDB.Extensions.FilteredIndex/Program.cs
+++ MongoDB.Extensions.FilteredIndex/Program.cs
@@ -1,5 +1,7 @@
-﻿using MongoDB.Bson.Serialization;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 using MongoDB.Extensions.FilteredIndex;
 using Testcontainers.MongoDb;
 
@@ -23,7 +25,33 @@ await collection.InsertManyAsync(
     }
 );
 
+await collection.UpdateManyAsync(
+    Builders<Foo>.Filter.Empty,
+    Builders<Foo>.Update.Set("elem", x => x.Scores, 100),
+    new UpdateOptions
+    {
+        ArrayFilters = new ArrayFilterDefinition[]
+        {
+            Builders<int>.Filter.ItemGte("elem", 100)
+        }
+    });
+
+foreach (var item in await collection.Find(Builders<Foo>.Filter.Empty).ToListAsync())
+{
+    Console.WriteLine(item.ToJson());
+}
+
 var set = Builders<Foo>.Update.Set("elem", x => x.Scores, 0);
 var content = set.Render(collection.DocumentSerializer, BsonSerializer.SerializerRegistry);
+
+Console.WriteLine(content);
+
+var equalFilter = Builders<int>.Filter.ItemEq("elem", 10);
+content = equalFilter.Render(BsonSerializer.LookupSerializer<int>(), BsonSerializer.SerializerRegistry, LinqProvider.V3);
+
+Console.WriteLine(content);
+
+var gteFilter = Builders<int>.Filter.ItemGte("elem", 90);
+content = gteFilter.Render(BsonSerializer.LookupSerializer<int>(), BsonSerializer.SerializerRegistry, LinqProvider.V3);
 
 Console.WriteLine(content);
